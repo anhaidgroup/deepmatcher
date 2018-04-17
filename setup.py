93c29ced94b8e5c9f394ff03b154d10586c1b4ee
Sidharth Mudgal <sidharthmsk@gmail.com>
2018-04-17 00:28:42 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='deepmatcher',
     description='A deep learning package for entity matching',
     version='0.0.1',
-    packages=['deepmatcher'],
+    packages=find_packages(),
     install_requires=['torch>=0.3.1', 'tqdm', 'pyprind', 'six', 'Cython', 'fasttext'],
     dependency_links=[
         'https://github.com/facebookresearch/tarball/master#fasttext-0.1.1'
