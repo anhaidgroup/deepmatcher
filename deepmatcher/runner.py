44c7ab8493cf2d2288b085420e1042d8e1eb27c0
belerico <belo.fede@outlook.com>
2019-06-15 22:46:24 +0200

    Fix bug output attributes on predicitons

--- deepmatcher/runner.py
+++ deepmatcher/runner.py
@@ -419,6 +419,7 @@ class Runner(object):
         if output_attributes:
             raw_table = pd.read_csv(dataset.path).set_index(dataset.id_field)
             raw_table.index = raw_table.index.astype('str')
-            pred_table = pred_table.join(raw_table)
+            # pred_table = pred_table.join(raw_table)
+            pred_table = pred_table + raw_table
 
         return pred_table
