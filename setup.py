f27c47eef93e49b440166a2e687fd1cfee16e8c1
belerico <belo.fede@outlook.com>
2019-07-19 14:13:40 +0200

    Change pytorch version

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     packages=['deepmatcher', 'deepmatcher.data', 'deepmatcher.models'],
     python_requires='>=3.5',
     install_requires=[
-        'torch==1.1.0', 'tqdm', 'pyprind', 'six', 'Cython', 'torchtext', 'nltk>=3.2.5',
+        'torch>=1.1.0', 'tqdm', 'pyprind', 'six', 'Cython', 'torchtext', 'nltk>=3.2.5',
         'pandas', 'requests', 'fasttextmirror', 'sklearn'
     ])
