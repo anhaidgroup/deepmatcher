53f57485bd8b25da6ac36035e3e9edf726ac9c3d
Sword-holder <iamthexzy@gmail.com>
2019-12-31 03:30:00 +0800

    Add sklearn into requirements (#41)

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
     python_requires='>=3.5',
     install_requires=[
         'torch>=1.0', 'tqdm', 'pyprind', 'six', 'Cython', 'torchtext', 'nltk>=3.2.5',
-        'fasttextmirror', 'pandas', 'dill'
+        'fasttextmirror', 'pandas', 'dill', 'scikit-learn'
     ])
